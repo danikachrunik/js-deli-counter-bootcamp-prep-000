538c4c760e504845b7b5808a4562fdef24270352
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-25 23:37:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
+
+function nowServing() {
+  if(katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    return katzDeliLine.shift();
+  }
+}
