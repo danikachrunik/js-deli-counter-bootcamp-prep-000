1e3ca153d4717311657e562fb59b57aebf61527a
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-25 23:34:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function takeANumber(katzDeliLine, name) {
-  if(katzDeliLine.length === 0) {
-    return `Welcome, ${name}. You are number 1 in line.`;
-  } else {
-    return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
-  }
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
