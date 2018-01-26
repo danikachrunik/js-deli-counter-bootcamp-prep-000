c6204dadf3c85abd1a687b09947c3e247811991d
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-26 01:21:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLin(line) {
+function currentLine(line) {
   if(line.length === 0) {
     return "The line is currently empty.";
   } else if(line.length === 1) {
     return `The line is currently: 1. ${line[0]}`;
   } else {
-    for(let i = 0; i < line.length; i++) {
-      
+    let str = `The line is currently: 1. ${line[0]}`;
+    for(let i = 1; i < line.length; i++) {
+      str += `, ${i + 1}. ${line[i]}`;
     }
+    return str;
   }
 }
